615907723f7ae1759f2af5e37af0c4547bab2da6
Abhishek Takle <157615952+Abhee0@users.noreply.github.com>
2025-11-19 14:46:49 +0530

    Fixed Project

--- Adarsh Code/vms-ui/src/components/common/SearchInput.jsx
+++
@@ -1,8 +0,0 @@
-export default function SearchInput({ placeholder }) {
-  return (
-    <div className="search">
-      <span className="search-icon">🔍</span>
-      <input placeholder={placeholder} />
-    </div>
-  );
-}
